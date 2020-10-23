5e3a5947a42370929ad769d0fc8a4b3c24c9f16c
victor.penit <vpen01@ucm.es>
2020-10-23 23:47:15 +0200

    para esteblecer realizadores

--- src/main/java/juegos/cartas/poker/juego/fact/FactPoker.java
+++ src/main/java/juegos/cartas/poker/juego/fact/FactPoker.java
@@ -1,15 +1,20 @@
 package juegos.cartas.poker.juego.fact;
 
+import java.util.List;
+
 import juegos.cartas.cartas.cartas.CartaFrancesaOld;
 import juegos.cartas.cartas.cartas.modelos.ICartaNumeroPaloFrancesa;
 import juegos.cartas.cartas.cartas.palos.PaloFrances;
 import juegos.cartas.cartas.mazos.fact.FactMazoConcretoEnums;
 import juegos.cartas.cartas.mazos.impl.gen.MazoGen;
 import juegos.cartas.poker.juego.JuegoPoker;
+import juegos.cartas.poker.juego.JugadorPokerTexasHoldem;
+import juegos.cartas.poker.juego.realizadorTurno.FactRealizadorPoker;
+import juegos.cartas.poker.juego.realizadorTurno.RealizadorTurnoPoker;
 
 public class FactPoker{
 
-	
+	FactRealizadorPoker factRealizador= new FactRealizadorPoker();
 	public JuegoPoker<ICartaNumeroPaloFrancesa<Integer,PaloFrances>> creaPoker(int j)
 	{
 		FactMazoConcretoEnums factMazo= new FactMazoConcretoEnums();
@@ -17,6 +22,15 @@ public class FactPoker{
 		
 		JuegoPoker<ICartaNumeroPaloFrancesa<Integer,PaloFrances>> poker=
 				new JuegoPoker<>(j, mazo);
+		
+		List<JugadorPokerTexasHoldem<ICartaNumeroPaloFrancesa<Integer,PaloFrances>>> jugadores= poker.getMesa().getJugadores();
+		
+		for (JugadorPokerTexasHoldem<ICartaNumeroPaloFrancesa<Integer, PaloFrances>> jugadorPokerTexasHoldem : jugadores)
+		{
+			
+			RealizadorTurnoPoker realizador= factRealizador.creaRealizador();
+			poker.estableceRealizadorParaJugador(jugadorPokerTexasHoldem,realizador);
+		}
 		
 		return poker;
 	}
---
+++ src/main/java/juegos/cartas/poker/juego/realizadorTurno/FactRealizadorPoker.java
@@ -0,0 +1,12 @@
+package juegos.cartas.poker.juego.realizadorTurno;
+
+import juegos.cartas.cartas.cartas.ICartaComparable;
+
+public class FactRealizadorPoker<C extends ICartaComparable>
+{
+
+	public RealizadorTurnoPoker<C> creaRealizador()
+	{
+		return null;
+	}
+}
